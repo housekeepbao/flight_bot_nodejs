929695cc30a4b95f41ae860be86b612e129cffa7
JHANG JHIH KAI <eric_jhang@flightgoai.com>
2018-08-27 14:30:17 +0800

    fixed  declared

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 
 // create LINE SDK config from env variables
+/*
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
+*/
 const config = require('./lineConfig.js').lineAccoessTokenConfig
 
 // create LINE SDK client
