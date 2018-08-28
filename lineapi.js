0353b78c09eedb6b5690c17d98bbd1d6333af61e
JHANG JHIH KAI <eric_jhang@flightgoai.com>
2018-08-28 15:28:50 +0800

    fixed bug

--- lineapi.js
+++ lineapi.js
@@ -1,3 +1,18 @@
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+/*
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+*/
+const config = require('./lineConfig.js').lineAccoessTokenConfig
+
+// create LINE SDK client
+const client = new line.Client(config);
+
 module.exports = {}
 module.exports.replyText = function (event, replay_text) {
     client.replyMessage(event.replyToken, replay_text)
